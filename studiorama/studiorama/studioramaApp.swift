c8dce461fa72edefbd924f178f643b51a08695dc
Italia <cardullo.lia@gmail.com>
2025-04-21 12:28:18 +0200

    Start: Initial Project

---
+++ studiorama/studiorama/studioramaApp.swift
@@ -0,0 +1,17 @@
+//
+//  studioramaApp.swift
+//  studiorama
+//
+//  Created by Italia Maria Desiré Cardullo on 21/04/25.
+//
+
+import SwiftUI
+
+@main
+struct studioramaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
